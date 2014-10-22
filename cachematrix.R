2ad0fd57fedacf83a72058cad5e31e2ebfdcd8cd
Paul-MM <mallon-p@sky.com>
2014-10-22 16:00:15 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The function below enable R to cache potentially time consuming computations. 
+## Here a matrix & its inverses are cached using the superassignment operator.
 
-## Write a short comment describing this function
+## This function creates a special matrix that will cashe its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL                                 ###
+  set <- function(y) {                      ###
+    x <<- y                                 ### variable x assigned with the superassignment operator                  
+    m <<- NULL
+  }
+  get <- function() x
+  setmatrix <- function(solve) m <<- solve
+  getmatrix <- function() m
+  list(set = set, get = get,
+       setmatrix = setmatrix,
+       getmatrix = getmatrix)
 
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), 
+##  then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getmatrix()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  matrix <- x$get()
+  m <- solve(matrix, ...)
+  x$setmatrix(m)
+  m     ## Return a matrix that is the inverse of 'x'
 }
